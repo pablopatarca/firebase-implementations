50aba5e3bc4eb4284bf85cea5413fcc07708ebb2
Pablo <pablo.patarca@ecloudsolutions.com>
2016-07-29 19:34:48 -0300

    add new project with push notifications implemented using firebase

---
+++ app/src/main/java/com/pablopatarca/firebaseimplementation/push/MessagingService.java
@@ -0,0 +1,53 @@
+package com.pablopatarca.firebaseimplementation.push;
+
+import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.content.Context;
+import android.content.Intent;
+import android.media.RingtoneManager;
+import android.net.Uri;
+import android.support.v4.app.NotificationCompat;
+import android.util.Log;
+
+import com.google.firebase.messaging.FirebaseMessagingService;
+import com.google.firebase.messaging.RemoteMessage;
+import com.pablopatarca.firebaseimplementation.MainActivity;
+import com.pablopatarca.firebaseimplementation.R;
+
+/**
+ * Created by Pablo on 13/7/16.
+ */
+
+public class MessagingService extends FirebaseMessagingService {
+
+    @Override
+    public void onMessageReceived(RemoteMessage remoteMessage) {
+        // TODO(developer): Handle FCM messages here.
+        // If the application is in the foreground handle both data and notification messages here.
+        // Also if you intend on generating your own notifications as a result of a received FCM
+        // message, here is where that should be initiated. See sendNotification method below.
+        Log.d(getClass().getSimpleName(), "From: " + remoteMessage.getFrom());
+        Log.d(getClass().getSimpleName(), "Notification Message Body: " + remoteMessage.getNotification().getBody());
+
+        sendNotification(remoteMessage.getNotification().getBody());
+    }
+
+    private void sendNotification(String messageBody) {
+        Intent intent = new Intent(this, MainActivity.class);
+        intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+        PendingIntent pendingIntent = PendingIntent.getActivity(this, 0 /* Request code */, intent, PendingIntent.FLAG_ONE_SHOT);
+
+        Uri defaultSoundUri= RingtoneManager.getDefaultUri(RingtoneManager.TYPE_NOTIFICATION);
+        NotificationCompat.Builder notificationBuilder = new NotificationCompat.Builder(this)
+                .setSmallIcon(R.drawable.ic_menu_gallery)
+                .setContentText(messageBody)
+                .setAutoCancel(true)
+                .setSound(defaultSoundUri)
+                .setVibrate(new long[] { 200, 200, 200 })
+                .setContentIntent(pendingIntent);
+
+        NotificationManager notificationManager = (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
+
+        notificationManager.notify(0 /* ID of notification */, notificationBuilder.build());
+    }
+}
